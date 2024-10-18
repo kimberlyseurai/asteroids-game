1f664da41fcf8d23a373acbe47b2cf153f22dcf1
kimberlyseurai <kimberlyseurai@gmail.com>
2024-10-18 14:34:13 -0400

    updated

--- main.py
+++ main.py
@@ -3,19 +3,25 @@
 # throughout this file
 import pygame
 from constants import *
+from player import *
 
 
 def main():
+    
+    newPlayer = Player((SCREEN_WIDTH / 2), (SCREEN_HEIGHT / 2))
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    py_Clock = pygame.time.Clock()
+    dt = 0
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 return
-        screen.fill(color=(0,0,0))
+        screen.fill(color=(255, 182, 193))
+        newPlayer.draw(screen)
         pygame.display.flip()
-
+        dt = py_Clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
