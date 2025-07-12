ad69069e63128d0eff764e5ac0c654bf62764a82
kimberlyseurai <kimberlyseurai@gmail.com>
2025-07-12 12:52:57 -0400

    working triangle

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
-# this allows us to use code from
-# the open-source pygame library
-# throughout this file
+
 import pygame
 from constants import *
+from player import Player
 
-
-print("Starting Asteroids!")
-print("Screen width:", SCREEN_WIDTH)
-print("Screen height:", SCREEN_HEIGHT)
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    clock = pygame.time.Clock()
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    Player.containers = (updatable, drawable)
+    dt = 0
+    player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
 
     while True:
         for event in pygame.event.get():
@@ -19,8 +20,19 @@ def main():
                 pygame.quit()
                 return
         
-        screen.fill((0, 0, 0))  # Fill screen with black
+       
+        updatable.update(dt)
+        screen.fill("pink")  # Fill screen with black
+
+        for obj in drawable:
+            obj.draw(screen)
+
         pygame.display.flip()   # Update the screen
+        
+        dt = clock.tick(60) / 1000
+        
+        
+        
 
 if __name__ == "__main__":
     main()
