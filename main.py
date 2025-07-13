3b6c12fb0c1dc32343281205d262d1b5342fea68
kimberlyseurai <kimberlyseurai@gmail.com>
2025-07-13 09:03:10 -0400

    collides_with

--- main.py
+++ main.py
@@ -1,18 +1,32 @@
 
 import pygame
+import sys
 from constants import *
 from player import Player
-
+from asteroid import Asteroid
+from asteroidfield import *
+from circleshape import CircleShape
 
 def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
     clock = pygame.time.Clock()
+    
     updatable = pygame.sprite.Group()
     drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = (updatable) 
+    asteroids_field = AsteroidField()
+    
     Player.containers = (updatable, drawable)
-    dt = 0
+
     player = Player(SCREEN_WIDTH / 2, SCREEN_HEIGHT / 2)
+
+    dt = 0
+    
+
 
     while True:
         for event in pygame.event.get():
@@ -22,6 +36,14 @@ def main():
         
        
         updatable.update(dt)
+
+        for asteroid in asteroids:
+            if asteroid.collides_with(player):
+                print("Game over!")
+                sys.exit()
+
+
+
         screen.fill("pink")  # Fill screen with black
 
         for obj in drawable:
